8fa8bbea12b8c0d41d24ed978634ce05a0d6061c
brahimelhabchi09-lgtm <brahimelhabchi09@gmail.com>
2026-01-25 04:01:17 +0100

    feat: Implement a layered architecture with new models, mappers, repositories, services, controllers, and views for various user roles, alongside database, routing, and authentication middleware.

--- public/index.php
+++ public/index.php
@@ -1,13 +1,7 @@
 <?php
 
-use Core\Router\Router;
+require_once __DIR__ . "/../vendor/autoload.php";
 
-require_once __DIR__ ."/../vendor/autoload.php";
-$router = new  Router() ; 
-$router->get("/", "HomeController@index");
-$router->get("/login", "AuthController@login");
-$router->get("/admin", "AdminController@index");
-$router->get("/Formateur", "FormateurController@index");
-$router->get("/Student", "StudentController@index");
+session_start();
 
-$router->dispatch();
+require_once __DIR__ . "/../route/web.php";
