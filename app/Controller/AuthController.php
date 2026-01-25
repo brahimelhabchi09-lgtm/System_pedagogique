3af5a42ebc38783850eaad3dd2df94d429c430e5
brahimelhabchi09-lgtm <brahimelhabchi09@gmail.com>
2026-01-25 03:55:09 +0100

    feat: Implement a layered architecture with new models, mappers, repositories, services, controllers, and views for various user roles, alongside database, routing, and authentication middleware.

--- app/Controller/AuthController.php
+++ app/Controller/AuthController.php
@@ -1,5 +1,8 @@
 <?php
+
 namespace App\Controller;
+
+use Core\Database\Database;
 
 class AuthController extends Controller
 {
@@ -7,4 +10,49 @@ class AuthController extends Controller
     {
         return $this->view("Auth.login");
     }
+
+    public function submitLogin()
+    {
+        if (!isset($_POST["email"]) || !isset($_POST["password"])) {
+            return $this->view("Auth.login", [
+                "error" => "Please provide both email and password"
+            ]);
+        }
+
+        $query = "SELECT * FROM users WHERE email = :email";
+
+        $stmt = Database::getConnection()->prepare($query);
+        $stmt->execute([
+            "email" => $_POST["email"]
+        ]);
+
+        $user = $stmt->fetch();
+
+        if ($user && password_verify($_POST["password"], $user["password"])) {
+            $_SESSION["user"] = $user;
+
+            if ($user['role'] === 'ADMIN') {
+                header("Location: /admin");
+            } elseif ($user['role'] === 'TEACHER') {
+                header("Location: /Formateur");
+            } elseif ($user['role'] === 'STUDENT') {
+                header("Location: /Student");
+            } else {
+                header("Location: /");
+            }
+            exit;
+        }
+
+        return $this->view("Auth.login", [
+            "error" => "Invalid email or password"
+        ]);
+    }
+    public function logout()
+    {
+        $_SESSION = [];
+        session_destroy();
+        header("Location: /");
+        exit;
+    }
+
 }
