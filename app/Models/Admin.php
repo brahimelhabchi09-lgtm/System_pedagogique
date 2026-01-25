a6d16a39ffe01c9cc7628c0b0e83c6262a972324
brahimelhabchi09-lgtm <brahimelhabchi09@gmail.com>
2026-01-25 03:57:51 +0100

    feat: Implement a layered architecture with new models, mappers, repositories, services, controllers, and views for various user roles, alongside database, routing, and authentication middleware.

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -1,4 +1,12 @@
 <?php
+
+use App\Models\User;
+
 class Admin extends User {
-    
+    private $role;
+
+    public function __construct($firstName, $lastName, $email, $password , $role = 'ADMIN') {
+        parent::__construct($firstName, $lastName, $email, $password);
+        $this->role = $role;
+    }
 }
