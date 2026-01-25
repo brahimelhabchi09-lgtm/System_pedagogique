196b57aede8e57137440b24428b34f492679d934
brahimelhabchi09-lgtm <brahimelhabchi09@gmail.com>
2026-01-25 03:58:50 +0100

    feat: Implement a layered architecture with new models, mappers, repositories, services, controllers, and views for various user roles, alongside database, routing, and authentication middleware.

--- app/Models/Teacher.php
+++ app/Models/Teacher.php
@@ -1,4 +1,12 @@
 <?php
-class Teacher extends User {
-    
+namespace App\Models;
+
+class Teacher extends User
+{
+    private $role;
+    public function __construct($firstName, $lastName, $email, $password, $phone = null, $age = null, $createdAt = null, $id = null, $role = 'TEACHER')
+    {
+        parent::__construct($firstName, $lastName, $email, $password, $phone, $age, $createdAt, $id);
+        $this->role = $role;
+    }
 }
