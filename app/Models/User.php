7fa4e8b19d37d2ec0cf0a5bf59372cd153f87afb
brahimelhabchi09-lgtm <brahimelhabchi09@gmail.com>
2026-01-25 03:58:55 +0100

    feat: Implement a layered architecture with new models, mappers, repositories, services, controllers, and views for various user roles, alongside database, routing, and authentication middleware.

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,4 +1,63 @@
 <?php
- class  User {
-    
- }
+namespace App\Models;
+
+class User
+{
+    protected $id;
+    protected $firstName;
+    protected $lastName;
+    protected $email;
+    protected $password;
+    protected $phone;
+    protected $age;
+    protected $createdAt;
+
+    public function __construct($firstName, $lastName, $email, $password, $phone = null, $age = null, $createdAt = null, $id = null)
+    {
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+        $this->email = $email;
+        $this->password = $password;
+        $this->phone = $phone;
+        $this->age = $age;
+        $this->createdAt = $createdAt;
+        $this->id = $id;
+    }
+
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+    public function getEmail()
+    {
+        return $this->email;
+    }
+    public function getPassword()
+    {
+        return $this->password;
+    }
+    public function getId()
+    {
+        return $this->id;
+    }
+    public function getPhone()
+    {
+        return $this->phone;
+    }
+    public function getAge()
+    {
+        return $this->age;
+    }
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+}
